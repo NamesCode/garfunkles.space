8594f45f1724206017dbca0a8bd835375078d4f2
Name <namesexistsinemails@gmail.com>
2023-04-01 19:36:55 +0100

    Mock of webpage V0.1.0

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,96 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<html lang="en-GB">
+    <body>
+        <div class="card" style="float: left; width: 100%;">
+            <div class="padded">
+                <div class="navbar">
+                    <h3>My website</h3>
+                    <p>text</p>
+                    <p>text</p>
+                    <p>text</p>
+                </div>
+            </div>
+        </div>
+        <div style="clear:both;"></div>
+        <div class="garf_line" style="margin-bottom: 5px;"></div>
+        <div class="card" style="float: left; width: 90%">
+            <div class="padded">
+                <div class="name">
+                    <img src="garfpfp.png" alt="PFP" height=90px width=90px>
+                    <h1>Name</h1>
+                    <hr>
+                </div>
+                <div style="clear:both;"></div>
+                <img src="Apple_Computer_Logo_rainbow.svg"
+                     height="200px"
+                     width="200px"
+                     alt="Apple logo"
+                     style="float: right;"/>
+                <p>This is filler text.
+
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer sagittis molestie quam, eget convallis orci. Nunc congue eros et ipsum aliquam egestas. Maecenas congue, quam nec dictum bibendum, est felis sagittis ex, mollis tristique augue lorem vitae nunc. Duis id turpis nec mi semper tempor eget in orci. Aenean rhoncus justo quis magna ultricies convallis. Praesent in sem sit amet turpis tempus consectetur in id eros. Aliquam placerat velit ut eros tempor, sit amet fringilla enim volutpat. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nulla volutpat ultrices porta. Phasellus in velit lobortis, feugiat ante in, auctor diam. Curabitur ullamcorper nec ante a consectetur.
+
+                    Suspendisse volutpat lorem vel ullamcorper finibus. Nulla id lectus id massa varius cursus. Nunc porta vel dolor non tristique. Integer tempor porta mi vitae facilisis. Mauris malesuada laoreet lobortis. Donec a diam blandit, dictum turpis a, imperdiet lorem. Vestibulum vitae condimentum eros. Curabitur leo dolor, malesuada vel felis ac, hendrerit pretium mi. Vestibulum fermentum, augue ac ultrices congue, ipsum justo consequat est, eget suscipit sem ante quis risus. Morbi posuere nunc tellus, eget semper lectus congue nec. Sed sodales mi ut quam condimentum euismod. Nam lacus lectus, venenatis in imperdiet vitae, maximus ac diam. Maecenas tempor turpis at cursus consequat. Sed venenatis dolor sed pellentesque consectetur. Cras euismod urna tincidunt dui hendrerit, vitae imperdiet est bibendum. Vestibulum dignissim rhoncus velit quis aliquam.
+
+                    Quisque eu laoreet erat, non sollicitudin arcu. Cras a hendrerit nunc. Vivamus interdum leo ut hendrerit finibus. Curabitur faucibus commodo efficitur. Nullam faucibus vel nunc eget accumsan. Cras euismod varius rhoncus. Donec non metus eu est euismod vehicula.
+
+                    Phasellus tempor mauris non eleifend ullamcorper. Quisque non orci vitae magna tristique porttitor et eu ante. Phasellus gravida sollicitudin sapien, sit amet tempor tellus ornare sit amet. Fusce ornare in nisi vel aliquet. Nulla vitae aliquet arcu. Nam magna sapien, suscipit fringilla tempus vitae, convallis rhoncus tortor. Cras viverra erat ut dui gravida, at egestas lorem sagittis. Aliquam egestas dolor enim, id interdum ligula dignissim ac. Donec finibus ultricies ullamcorper. Etiam id nisl sit amet est faucibus laoreet non ut lectus.
+
+                    Cras nec egestas dolor, faucibus pulvinar odio. Aliquam erat volutpat. Aenean vel justo non nisl venenatis venenatis. Sed ullamcorper, diam id viverra volutpat, urna magna rhoncus orci, vel fermentum tortor tellus eget ante. Integer in sem eu turpis tempus finibus sit amet iaculis erat. Etiam turpis orci, ornare nec metus ac, fermentum ultrices nisi. Morbi luctus venenatis elit eget accumsan. </p>
+
+            </div>
+        </div>
+        <div style="float: right; margin: 5px;">
+            <img src="cokeva.gif" alt="Coke Vanilla">
+        </div>
+    </body>
+</html>
+
+<style>
+ html {
+	 background-image: url('stars.gif');
+ }
+ .card {
+     background-color: #181825;
+     border: 2.5px solid #f38ba8;
+     border-radius: 5px;
+     margin-bottom: 5px;
+ }
+ hr {
+     color: #bac2de;
+     width: 90%;
+     padding: 0;
+ }
+ h1 {
+     color: #bac2de;
+     font-weight: normal;
+     font-family: "Rockwell", serif;
+ }
+ p, h3 {
+     color: #bac2de;
+     font-family: "Lucida Console", "Courier New", monospace;
+     text-align: justify;
+ }
+ .padded {
+     padding-left: 1vw;
+     /* padding-right: 1vw; */
+ }
+ .garf_line {
+     background-image: url('garf_line2.gif');
+     background-repeat: repeat-x;
+     height: 2rem;
+ }
+ .name {
+     padding-top: 1vw;
+ }
+ .name * {
+     float: left;
+ }
+ .name img {
+     border-radius: 20%;
+     padding-right: 7.5px;
+ }
+ .navbar * {
+     padding-left: 5px;
+     float: left;
+ }
+</style>
