ba1368609e5dfde885f6543c2f50e65dceacd2ef
Name <namesexistsinemails@gmail.com>
2023-04-02 04:44:06 +0100

    Move navbar to +layout.svelte + Improvements

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,96 +1,94 @@
-<html lang="en-GB">
-    <body>
-        <div class="card" style="float: left; width: 100%;">
-            <div class="padded">
-                <div class="navbar">
-                    <h3>My website</h3>
-                    <p>text</p>
-                    <p>text</p>
-                    <p>text</p>
+        <body>
+            <div class="card">
+                <div class="padded">
+                    <div class="name">
+                        <img src="garfpfp.png" alt="PFP" height=90px width=90px>
+                        <h1>Name</h1>
+                        <hr>
+                    </div>
+                    <div style="clear:both;"></div>
+                    <div class="content">
+                    <img src="Apple_Computer_Logo_rainbow.svg"
+                         height="200px"
+                         width="200px"
+                         alt="Apple logo"
+                         style="float: right;"/>
+                    <p>This is filler text.
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi non finibus orci, sit amet imperdiet lacus. Phasellus vitae sodales tellus. Sed non lacus vel ante tincidunt pellentesque a sit amet orci. Nam sollicitudin et elit et lobortis. Fusce a purus dictum, tincidunt orci quis, malesuada arcu. Aenean justo mi, accumsan a ultricies ac, porta sit amet leo. Curabitur pulvinar nisl eu nisi consequat sollicitudin. Donec aliquet quis purus eu porta. Donec mattis sapien sed felis convallis tempor. Aenean imperdiet massa diam, sit amet cursus erat faucibus non. Maecenas placerat turpis at augue malesuada, et vehicula nulla efficitur. Vivamus cursus fermentum suscipit. </p>
+                    <p>Integer sollicitudin, dui ut laoreet porta, mauris justo finibus ante, nec eleifend ligula neque eget tellus. Nulla eu tempus enim. Phasellus pretium vel velit sodales pellentesque. In non iaculis justo. Nam facilisis purus quis quam varius imperdiet. Phasellus rutrum efficitur elit in aliquet. Curabitur vulputate lorem in tellus rhoncus facilisis. Integer pellentesque interdum fermentum. Curabitur consequat, est ullamcorper iaculis tempor, tellus ligula tempus libero, sed commodo sapien felis at metus. Suspendisse ultrices elit sit amet purus viverra, porta imperdiet sapien efficitur. Nullam fermentum commodo nibh, eu accumsan felis. Nulla et lectus lectus. Cras ac turpis velit. Praesent lectus ante, interdum quis sodales a, aliquet vitae dolor. </p>
+                    <p>Aliquam in metus ac dui blandit facilisis vel vel lorem. Praesent at lacus ultricies, rutrum elit ac, venenatis diam. Sed aliquam vulputate commodo. Donec a nisl id lectus rutrum placerat. Quisque sodales nisi magna, eu pharetra diam laoreet a. Pellentesque rhoncus eu lectus in posuere. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Quisque non interdum tellus. Nunc euismod, ante non porta efficitur, lacus arcu dictum lectus, nec accumsan dui risus et magna. </p>
+                    <p>Etiam sit amet feugiat erat, sit amet vestibulum arcu. Praesent risus ex, lobortis sit amet pellentesque ac, dignissim in felis. Donec condimentum eros et nisl fermentum scelerisque. Phasellus condimentum mauris nunc, id aliquet nulla eleifend a. Donec rhoncus tristique ligula, quis tempus orci. In finibus enim vitae vehicula dictum. Nullam pharetra porta enim, in vehicula lorem laoreet et. Pellentesque tristique vulputate viverra. Suspendisse pharetra fermentum sagittis. Vestibulum vel placerat risus, in gravida tellus. </p>
+                    <p>Donec a augue vitae metus imperdiet iaculis non vitae mi. Morbi condimentum nisi leo, ut fermentum justo pharetra sit amet. Quisque pretium ullamcorper porta. Aliquam semper, ipsum et faucibus vulputate, dui sapien dapibus nibh, non molestie tellus nisi id nisi. Nulla facilisi. Nullam sit amet eleifend sapien. Nulla quis tortor nisi. Aliquam erat volutpat. Suspendisse lacinia laoreet velit, et tincidunt nibh luctus in. Ut eu erat tortor. Phasellus ut metus urna. Aliquam consequat vel nisi eu maximus. Sed non porttitor mi, nec lobortis mi. Cras sed turpis sed lectus aliquet egestas. Cras mi magna, accumsan at pretium ut, euismod eu nisi. Aliquam nec odio sit amet ipsum molestie mollis at pulvinar odio. </p>
+                    </div>
                 </div>
             </div>
-        </div>
-        <div style="clear:both;"></div>
-        <div class="garf_line" style="margin-bottom: 5px;"></div>
-        <div class="card" style="float: left; width: 90%">
-            <div class="padded">
-                <div class="name">
-                    <img src="garfpfp.png" alt="PFP" height=90px width=90px>
-                    <h1>Name</h1>
-                    <hr>
-                </div>
-                <div style="clear:both;"></div>
-                <img src="Apple_Computer_Logo_rainbow.svg"
-                     height="200px"
-                     width="200px"
-                     alt="Apple logo"
-                     style="float: right;"/>
-                <p>This is filler text.
-
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer sagittis molestie quam, eget convallis orci. Nunc congue eros et ipsum aliquam egestas. Maecenas congue, quam nec dictum bibendum, est felis sagittis ex, mollis tristique augue lorem vitae nunc. Duis id turpis nec mi semper tempor eget in orci. Aenean rhoncus justo quis magna ultricies convallis. Praesent in sem sit amet turpis tempus consectetur in id eros. Aliquam placerat velit ut eros tempor, sit amet fringilla enim volutpat. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nulla volutpat ultrices porta. Phasellus in velit lobortis, feugiat ante in, auctor diam. Curabitur ullamcorper nec ante a consectetur.
-
-                    Suspendisse volutpat lorem vel ullamcorper finibus. Nulla id lectus id massa varius cursus. Nunc porta vel dolor non tristique. Integer tempor porta mi vitae facilisis. Mauris malesuada laoreet lobortis. Donec a diam blandit, dictum turpis a, imperdiet lorem. Vestibulum vitae condimentum eros. Curabitur leo dolor, malesuada vel felis ac, hendrerit pretium mi. Vestibulum fermentum, augue ac ultrices congue, ipsum justo consequat est, eget suscipit sem ante quis risus. Morbi posuere nunc tellus, eget semper lectus congue nec. Sed sodales mi ut quam condimentum euismod. Nam lacus lectus, venenatis in imperdiet vitae, maximus ac diam. Maecenas tempor turpis at cursus consequat. Sed venenatis dolor sed pellentesque consectetur. Cras euismod urna tincidunt dui hendrerit, vitae imperdiet est bibendum. Vestibulum dignissim rhoncus velit quis aliquam.
-
-                    Quisque eu laoreet erat, non sollicitudin arcu. Cras a hendrerit nunc. Vivamus interdum leo ut hendrerit finibus. Curabitur faucibus commodo efficitur. Nullam faucibus vel nunc eget accumsan. Cras euismod varius rhoncus. Donec non metus eu est euismod vehicula.
-
-                    Phasellus tempor mauris non eleifend ullamcorper. Quisque non orci vitae magna tristique porttitor et eu ante. Phasellus gravida sollicitudin sapien, sit amet tempor tellus ornare sit amet. Fusce ornare in nisi vel aliquet. Nulla vitae aliquet arcu. Nam magna sapien, suscipit fringilla tempus vitae, convallis rhoncus tortor. Cras viverra erat ut dui gravida, at egestas lorem sagittis. Aliquam egestas dolor enim, id interdum ligula dignissim ac. Donec finibus ultricies ullamcorper. Etiam id nisl sit amet est faucibus laoreet non ut lectus.
-
-                    Cras nec egestas dolor, faucibus pulvinar odio. Aliquam erat volutpat. Aenean vel justo non nisl venenatis venenatis. Sed ullamcorper, diam id viverra volutpat, urna magna rhoncus orci, vel fermentum tortor tellus eget ante. Integer in sem eu turpis tempus finibus sit amet iaculis erat. Etiam turpis orci, ornare nec metus ac, fermentum ultrices nisi. Morbi luctus venenatis elit eget accumsan. </p>
-
+            <div style="float: right; margin: -2.5px">
+                <img src="cokeva.gif" alt="Coke Vanilla" id="side-panel">
             </div>
-        </div>
-        <div style="float: right; margin: 5px;">
-            <img src="cokeva.gif" alt="Coke Vanilla">
-        </div>
-    </body>
-</html>
+        </body>
 
-<style>
- html {
-	 background-image: url('stars.gif');
- }
- .card {
-     background-color: #181825;
-     border: 2.5px solid #f38ba8;
-     border-radius: 5px;
-     margin-bottom: 5px;
- }
- hr {
-     color: #bac2de;
-     width: 90%;
-     padding: 0;
- }
- h1 {
-     color: #bac2de;
-     font-weight: normal;
-     font-family: "Rockwell", serif;
- }
- p, h3 {
-     color: #bac2de;
-     font-family: "Lucida Console", "Courier New", monospace;
-     text-align: justify;
- }
- .padded {
-     padding-left: 1vw;
-     /* padding-right: 1vw; */
- }
- .garf_line {
-     background-image: url('garf_line2.gif');
-     background-repeat: repeat-x;
-     height: 2rem;
- }
- .name {
-     padding-top: 1vw;
- }
- .name * {
-     float: left;
- }
- .name img {
-     border-radius: 20%;
-     padding-right: 7.5px;
- }
- .navbar * {
-     padding-left: 5px;
-     float: left;
- }
-</style>
+
+        <style>
+         .card {
+             background-color: #181825;
+             border: 2.5px solid #f38ba8;
+             border-radius: 5px;
+             margin-bottom: 5px;
+             float: left;
+             width: 90%;
+         }
+         .content {
+             max-width: 60%;
+             margin: auto;
+         }
+         p {
+             margin-top: -20px;
+             padding-top: 0.5em;
+             color: #bac2de;
+             font-family: "Lucida Console", "Courier New", monospace;
+             text-align: justify;
+             /* padding-right: 5vw; */
+             /* padding-left: 20%; */
+         }
+         .padded {
+             padding-left: 1vw;
+         }
+         hr {
+             color: #bac2de;
+             background-color: #bac2de;
+             margin-left: 10px;
+             max-width: 90%;
+             height: 2.5px;
+             border-radius: 5px;
+             margin-top: -30px;
+         }
+         h1 {
+             color: #bac2de;
+             font-weight: normal;
+             font-family: "Rockwell", serif;
+         }
+         .name {
+             padding-top: 1vw;
+         }
+         .name img {
+             float: left;
+         }
+         .name img {
+             border-radius: 20%;
+             padding-right: 7.5px;
+         }
+         @media only screen and (max-width: 1026px) {
+             #side-panel {
+                 display: none;
+             }
+             .card {
+                 width: 100%;
+             }
+             .content {
+                 padding-right: 1vw;
+                 max-width: 100%;
+                 margin: 0;
+             }
+
+         }
+        </style>
