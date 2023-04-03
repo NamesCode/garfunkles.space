e96da67ac74b26936d13498b8e1f9a17d3d1b067
Name <namesexistsinemails@gmail.com>
2023-04-03 16:34:44 +0100

    remove filler and start making a main page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,94 +1,129 @@
-        <body>
-            <div class="card">
-                <div class="padded">
-                    <div class="name">
-                        <img src="garfpfp.png" alt="PFP" height=90px width=90px>
-                        <h1>Name</h1>
-                        <hr>
-                    </div>
-                    <div style="clear:both;"></div>
-                    <div class="content">
-                    <img src="Apple_Computer_Logo_rainbow.svg"
-                         height="200px"
-                         width="200px"
-                         alt="Apple logo"
-                         style="float: right;"/>
-                    <p>This is filler text.
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi non finibus orci, sit amet imperdiet lacus. Phasellus vitae sodales tellus. Sed non lacus vel ante tincidunt pellentesque a sit amet orci. Nam sollicitudin et elit et lobortis. Fusce a purus dictum, tincidunt orci quis, malesuada arcu. Aenean justo mi, accumsan a ultricies ac, porta sit amet leo. Curabitur pulvinar nisl eu nisi consequat sollicitudin. Donec aliquet quis purus eu porta. Donec mattis sapien sed felis convallis tempor. Aenean imperdiet massa diam, sit amet cursus erat faucibus non. Maecenas placerat turpis at augue malesuada, et vehicula nulla efficitur. Vivamus cursus fermentum suscipit. </p>
-                    <p>Integer sollicitudin, dui ut laoreet porta, mauris justo finibus ante, nec eleifend ligula neque eget tellus. Nulla eu tempus enim. Phasellus pretium vel velit sodales pellentesque. In non iaculis justo. Nam facilisis purus quis quam varius imperdiet. Phasellus rutrum efficitur elit in aliquet. Curabitur vulputate lorem in tellus rhoncus facilisis. Integer pellentesque interdum fermentum. Curabitur consequat, est ullamcorper iaculis tempor, tellus ligula tempus libero, sed commodo sapien felis at metus. Suspendisse ultrices elit sit amet purus viverra, porta imperdiet sapien efficitur. Nullam fermentum commodo nibh, eu accumsan felis. Nulla et lectus lectus. Cras ac turpis velit. Praesent lectus ante, interdum quis sodales a, aliquet vitae dolor. </p>
-                    <p>Aliquam in metus ac dui blandit facilisis vel vel lorem. Praesent at lacus ultricies, rutrum elit ac, venenatis diam. Sed aliquam vulputate commodo. Donec a nisl id lectus rutrum placerat. Quisque sodales nisi magna, eu pharetra diam laoreet a. Pellentesque rhoncus eu lectus in posuere. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Quisque non interdum tellus. Nunc euismod, ante non porta efficitur, lacus arcu dictum lectus, nec accumsan dui risus et magna. </p>
-                    <p>Etiam sit amet feugiat erat, sit amet vestibulum arcu. Praesent risus ex, lobortis sit amet pellentesque ac, dignissim in felis. Donec condimentum eros et nisl fermentum scelerisque. Phasellus condimentum mauris nunc, id aliquet nulla eleifend a. Donec rhoncus tristique ligula, quis tempus orci. In finibus enim vitae vehicula dictum. Nullam pharetra porta enim, in vehicula lorem laoreet et. Pellentesque tristique vulputate viverra. Suspendisse pharetra fermentum sagittis. Vestibulum vel placerat risus, in gravida tellus. </p>
-                    <p>Donec a augue vitae metus imperdiet iaculis non vitae mi. Morbi condimentum nisi leo, ut fermentum justo pharetra sit amet. Quisque pretium ullamcorper porta. Aliquam semper, ipsum et faucibus vulputate, dui sapien dapibus nibh, non molestie tellus nisi id nisi. Nulla facilisi. Nullam sit amet eleifend sapien. Nulla quis tortor nisi. Aliquam erat volutpat. Suspendisse lacinia laoreet velit, et tincidunt nibh luctus in. Ut eu erat tortor. Phasellus ut metus urna. Aliquam consequat vel nisi eu maximus. Sed non porttitor mi, nec lobortis mi. Cras sed turpis sed lectus aliquet egestas. Cras mi magna, accumsan at pretium ut, euismod eu nisi. Aliquam nec odio sit amet ipsum molestie mollis at pulvinar odio. </p>
-                    </div>
-                </div>
+<script>
+ export let foo;
+ foo = undefined;
+ // Values that are passed in as props
+ // are immediately available
+ console.log({ foo });
+</script>
+
+<body>
+    <div class="card" id="main-panel">
+        <div class="padded">
+            <div class="name">
+                <img src="garfpfp.png" alt="PFP" height=90px width=90px>
+                <h1>Name</h1>
+                <hr>
             </div>
-            <div style="float: right; margin: -2.5px">
-                <img src="cokeva.gif" alt="Coke Vanilla" id="side-panel">
+            <div style="clear:both;"></div>
+            <div class="content">
+                <img src="Apple_Computer_Logo_rainbow.svg"
+                     height="200px"
+                     width="200px"
+                     alt="Apple logo"
+                     style="float: right;"/>
+                <p>Hallo, Welcome to my webpage!</p>
+                <p>For the reasons of privacy I go under the pseudo names "Name" or "Garfunkle" online :)</p>
+                <p></p>
             </div>
-        </body>
+        </div>
+    </div>
+    <div id="side-panel">
+        <div class="card">
+            <h1 style="padding-left: 15px;">Socials</h1>
+            <hr>
+            <ul>Twitter: @NameIsALoser</ul>
+            <ul>Email: <a  href="mailto:namesexistsinemails@gmail.com">namesexistsinemails@gmail.com</a></ul>
+            <ul>Discord: NameExists#3898</ul>
+        </div>
+</body>
 
 
-        <style>
-         .card {
-             background-color: #181825;
-             border: 2.5px solid #f38ba8;
-             border-radius: 5px;
-             margin-bottom: 5px;
-             float: left;
-             width: 90%;
-         }
+<style>
+ a {
+     color: #f38ba8;
+     text-decoration: none;
+     font-family: "Lucida Console", "Courier New", monospace;
+ }
+ a:hover {
+     color: #eba0ac;
+ }
+ #side-panel {
+     float: right;
+     /* padding-left: 1vw; */
+     width: 20%;
+     padding-right: 24px;
+ }
+ #main-panel {
+     width: 77%;
+     padding-bottom: 1vw;
+ }
+ ul {
+     padding: 0;
+     padding-left: 1vw;
+     font-family: "Lucida Console", "Courier New", monospace;
+     padding-right: 1vw;
+ }
+ .card {
+     background-color: #181825;
+     border: 2.5px solid #f38ba8;
+     border-radius: 5px;
+     margin-bottom: 5px;
+     float: left;
+ }
+ .content {
+     max-width: 60%;
+     margin: auto;
+ }
+ ul {
+     color: #bac2de;
+ }
+ p {
+     margin-top: -20px;
+     padding-top: 0.5em;
+     color: #bac2de;
+     font-family: "Lucida Console", "Courier New", monospace;
+     text-align: justify;
+ }
+ .padded {
+     padding-left: 1vw;
+ }
+ hr {
+     color: #bac2de;
+     background-color: #bac2de;
+     margin-left: 10px;
+     max-width: 90%;
+     height: 2.5px;
+     border-radius: 5px;
+     margin-top: -30px;
+ }
+ h1 {
+     color: #bac2de;
+     font-weight: normal;
+     font-family: "Rockwell", serif;
+ }
+ .name {
+     padding-top: 1vw;
+ }
+ .name img {
+     float: left;
+ }
+ .name img {
+     border-radius: 20%;
+     padding-right: 7.5px;
+ }
+ @media only screen and (max-width: 1080px) {
+     #side-panel {
+         display: none;
+     }
+     #main-panel {
+         width: 100%;
+     }
+     @media only screen and (max-width: 1026px) {
          .content {
-             max-width: 60%;
-             margin: auto;
+             padding-right: 1vw;
+             max-width: 100%;
+             margin: 0;
          }
-         p {
-             margin-top: -20px;
-             padding-top: 0.5em;
-             color: #bac2de;
-             font-family: "Lucida Console", "Courier New", monospace;
-             text-align: justify;
-             /* padding-right: 5vw; */
-             /* padding-left: 20%; */
-         }
-         .padded {
-             padding-left: 1vw;
-         }
-         hr {
-             color: #bac2de;
-             background-color: #bac2de;
-             margin-left: 10px;
-             max-width: 90%;
-             height: 2.5px;
-             border-radius: 5px;
-             margin-top: -30px;
-         }
-         h1 {
-             color: #bac2de;
-             font-weight: normal;
-             font-family: "Rockwell", serif;
-         }
-         .name {
-             padding-top: 1vw;
-         }
-         .name img {
-             float: left;
-         }
-         .name img {
-             border-radius: 20%;
-             padding-right: 7.5px;
-         }
-         @media only screen and (max-width: 1026px) {
-             #side-panel {
-                 display: none;
-             }
-             .card {
-                 width: 100%;
-             }
-             .content {
-                 padding-right: 1vw;
-                 max-width: 100%;
-                 margin: 0;
-             }
-
-         }
-        </style>
+     }
+ }
+</style>
