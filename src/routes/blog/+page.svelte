0e824dbfbfdfb6819485f52e438701a88993d0aa
Name <namesexistsinemails@gmail.com>
2023-10-22 14:32:37 +0100

    Feat: Add blog

--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -1,27 +1,43 @@
 <script>
-    import attention from '$lib/assets/retro-os-icons/attention.png'
+    import { onMount } from "svelte";
+    import attention from '$lib/assets/retro-os-icons/attention.png';
+
+     //let postData;
+    async function getPostData() {
+     //onMount(async () => {
+     const postDataResponse = await fetch("/blog/api");
+     //const postData = await postDataResponse.json();
+     //console.log(postData);
+    //});
+    return await postDataResponse.json();
+    }
+
+    const postData = getPostData();
 </script>
 
-<div style="margin-top: 35vh; display: flex; align-items: center; padding-bottom: 1.5vh;">
+ <div style="margin-top: 35vh; display: flex; align-items: center; padding-bottom: 1.5vh;"> 
     <div style="align-items: center; margin: auto; padding-left: 1em; padding-right: 1em;">
         <div style="border: 2.5px double var(--text); background-color: var(--base);">
-            <span style="display: flex; align-items: center; justify-content: space-between;">
-                <p>Did you really expect some lame ass blog?</p>
-                <a href="https://youtu.be/5BZLz21ZS_Y" target="_blank" rel="noopener noreferrer">
-                    <img src="{attention}" alt="Attention!">
-                </a>
-            </span>
-            <p style="margin-top: -0.65em;">This website has literally only been up since 20/05/2023</p>
+        <main>
+                {#await postData}
+                {:then posts}
+                <ul>
+                {#each posts as {title, slug}}
+                <li>
+                <a href="/blog/{slug}">{title}</a>
+                </li>
+                {/each}
+                </ul>
+                {/await}
+                </main>
         </div>
     </div>
 </div>
 
 <style>
- p {
+ main {
+     align-items: center; 
      padding-left: 1em;
      padding-right: 1em;
  }
- img {
-     padding-right: 0.3em;
- }
-</style>
+ </style>
