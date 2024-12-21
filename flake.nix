14a4a9149ce1bf08c8913f16f9eb78d934f8023e
Name <lasagna@garfunkles.space>
2024-12-21 17:11:59 +0000

    Feat: Add Nix direnv

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  description = "Garfunkles.space: My shitty website. NOW IN VEL!";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+    }:
+    let
+      system = "aarch64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      devShells.aarch64-linux.default = pkgs.mkShell {
+        nativeBuildInputs = with pkgs; [
+          # Build tools
+          dart-sass
+          pandoc-cli
+          # velox
+
+          # LSPs
+          emmet-language-server
+          nodePackages.vscode-langservers-extracted
+        ];
+
+        shellHook = ''echo "You've now entered the devshell for Garfunkles.space. Build with ./build.sh"'';
+      };
+    };
+}
